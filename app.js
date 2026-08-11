016829839def46364cfa91a570c41a4bf4015cd4
simon-0113 <t0926400467@gmail.com>
2026-08-11 21:50:46 +0800

    Add team Dropbox upload

--- app.js
+++ app.js
@@ -754,7 +754,8 @@ $("#visitUpdateForm").addEventListener("submit", event => {
   currentStore = null;
 
   alert(editingId ? "今日巡店資料已修改。" : "已加入今日巡店。");
-  showView("sessionView", "今日巡店");
+  clearSearchState();
+  showView("homeView", "首頁");
 });
 
 $("#completeSessionButton").addEventListener("click", async () => {
